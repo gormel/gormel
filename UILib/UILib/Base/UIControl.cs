978ee6385310106db881423e9cbe88149b09a5bb
zackflame@gmail.com <zackflame@gmail.com@b7a93c86-6cec-942a-4d70-6915b0ce4462>
2013-06-18 22:24:42 +0000

    Added xml documentation for UIControl.

--- UILib/UILib/Base/UIControl.cs
+++ UILib/UILib/Base/UIControl.cs
@@ -8,44 +8,93 @@ using UILib.Utils;
 
 namespace UILib.Base
 {
-	public class UIControl
+    /// <summary>
+    /// Base class for all UI controls.
+    /// </summary>
+	public abstract class UIControl
 	{
-		public virtual float X 
-		{
-			get { return BaseControl.X + Margin; }
-			set {  }
-		}
-
+        /// <summary>
+        /// Gets or sets control's bounding box horizontal drawing offset in screen coordinates.
+        /// </summary>
+        public virtual float X
+        {
+            get { return BaseControl.X + Margin; }
+            set { throw new NotSupportedException(); }
+        }
+        /// <summary>
+        /// Gets or sets control's bounding box vertical drawing offset in screen coordinates.
+        /// </summary>
 		public virtual float Y
 		{
 			get { return BaseControl.Y + Margin; }
-			set {  }
+            set { throw new NotSupportedException(); }
 		}
-
+        /// <summary>
+        /// Gets or sets control's bounding box width.
+        /// </summary>
 		public virtual float Width
 		{
 			get { return BaseControl.Width - Margin * 2; }
-			set {  }
+            set { throw new NotSupportedException(); }
 		}
-
+        /// <summary>
+        /// Gets or sets control's bounding box height.
+        /// </summary>
 		public virtual float Height
 		{
 			get { return BaseControl.Height - Margin * 2; }
-			set {  }
+            set { throw new NotSupportedException(); }
 		}
 
+        private string name = "";
+
+        /// <summary>
+        /// Gets or sets a name of this control.
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException();
+                name = value;
+            }
+        }
+        /// <summary>
+        /// Specifies control's render box distance from it's bounding box.
+        /// </summary>
 		public float Margin { get; set; }
+        /// <summary>
+        /// Child controls of this control.
+        /// </summary>
 		public List<UIControl> Controls { get; private set; }
-		public string Name { get; set; }
+        /// <summary>
+        /// Parent control of this control.
+        /// </summary>
 		protected UIControl BaseControl { get; private set; }
+        /// <summary>
+        /// Graphics device associated with this control.
+        /// </summary>
 		protected GraphicsDevice GraphicsDevice { get; private set; }
+        /// <summary>
+        /// Sprite batch associated with this control.
+        /// </summary>
 		protected SpriteBatch SpriteBatch { get; private set; }
+
 		protected PrimetiveDrawHelper PrimetiveDarwHelper { get; private set; }
+        /// <summary>
+        /// Returns a value indicating whether control has almost zero-sized bounding box.
+        /// </summary>
 		protected bool IsNullSize { get { return Math.Abs(Width) < float.Epsilon || Math.Abs(Height) < float.Epsilon; } }
 
+        /// <summary>
+        /// Initializes a new UI control.
+        /// </summary>
+        /// <param name="baseControl">Parent of this control.</param>
+        /// <param name="device">Graphics device associated with this control.</param>
 		public UIControl(UIControl baseControl, GraphicsDevice device)
 		{
-			Name = "";
 			Margin = 0;
 			Controls = new List<UIControl>();
 			BaseControl = baseControl;
@@ -57,6 +106,10 @@ namespace UILib.Base
 				BaseControl.Controls.Add(this);
 		}
 
+        /// <summary>
+        /// Draws control using associated <see cref="GraphicsDevice"/>.
+        /// </summary>
+        /// <param name="time">Time elapsed from last draw invocation.</param>
 		public virtual void Draw(GameTime time)
 		{
 			if (IsNullSize)
@@ -66,6 +119,10 @@ namespace UILib.Base
 				Controls[i].Draw(time);
 			}
 		}
+        /// <summary>
+        /// Updates control state.
+        /// </summary>
+        /// <param name="time">Time elapsed from last update invocation.</param>
 		public virtual void Update(GameTime time)
 		{
 			for (int i = 0; i < Controls.Count; i++)
